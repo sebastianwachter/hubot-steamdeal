7f2676f17d8d10556b930be962d7d1f94b823be1
Sebastian Wachter <wachter@hydra-newmedia.com>
2016-01-25 16:55:46 +0100

    add new functionality (link and full)

--- steam-dailyapi.js
+++ steam-dailyapi.js
@@ -2,9 +2,11 @@
 
 const cheerio = require('cheerio');
 const got = require('got');
+const sanitize = require('sanitize-html');
 
 module.exports = (robot) => {
-  robot.hear('steamdeal', (msg) => {
+  robot.hear(/steamdeal (.*)/i, (msg) => {
+    var args = msg.match[1];
     got('http://store.steampowered.com').then((res) => {
       var body = res.body;
       if (res.statusCode !== 200) return msg.send('Steam currently unavailable!');
@@ -19,8 +21,19 @@ module.exports = (robot) => {
         let final = price.final / 100;
         let initial = price.initial / 100;
         let discount = price.discount_percent;
-        msg.send(`Instead of ${initial}€ you get ${name} for ${final}€! That\'s -${discount}%!`);
         msg.send(game.header_image);
+        msg.send(`Instead of ${initial}€ you get ${name} for ${final}€! That\'s -${discount}%!`);
+        if (args === 'link') {
+          msg.send(`https://http://store.steampowered.com/app/${id}`);
+        } else if (args === 'full') {
+          let description = game.detailed_description;
+          let clean = sanitize(description, {
+            allowedTags: [], 
+            allowedAttributes: {}
+          });
+          msg.send(`Description:\n${clean}`);
+          msg.send(`https://http://store.steampowered.com/app/${id}`);
+        }
       });
     });
   });
